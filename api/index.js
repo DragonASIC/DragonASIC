01df3934da4190a7e54179b849cd9f3507a5d544
Koki Takahashi <hakatasiloving@gmail.com>
2017-09-26 20:00:19 +0900

    Add code for docker runtime

--- api/index.js
+++ api/index.js
@@ -1,5 +1,9 @@
 const Koa = require('koa');
 const {get} = require('koa-route');
+const Docker = require('dockerode');
+const tmp = require('tmp');
+
+const docker = new Docker();
 
 const app = new Koa();
 
